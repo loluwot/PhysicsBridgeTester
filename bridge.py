4f26681b3327bcc611413861004cc52eb7f222de
Andy C <fontisawesome@gmail.com>
2020-05-26 10:56:56 -0400

    Added more bridge types.

--- bridge.py
+++ bridge.py
@@ -1,6 +1,7 @@
 from anastruct import SystemElements
 import anastruct
 import numpy as np
+import math
 #height = 0.136
 height = 0.15
 element_type = 'truss'
@@ -56,7 +57,7 @@ class Bridge:
     def mass(self):
         arr = self.ss.get_element_results()
         length_total = sum([x['length'] for x in arr])
-        return length_total * pop_mpl * 1000
+        return length_total * self.pop_mpl * 1000*1.1
     def show(self):
         self.ss.show_structure()
     def stress_test(self, dw):
@@ -68,7 +69,6 @@ class Bridge:
                 self.change_load(id, cur_load)
             cur_load += dw
         print("{} kg".format(cur_load))
-
 def createPratt(length, height, n):
     dx = length/n
     x = [i*dx for i in range(n+1)] + [i*dx for i in range(1,n)]
@@ -105,7 +105,45 @@ def createKTruss(length, height, n):
     supports = [(0), (n)]
     loads = [(math.floor((5/2)*n-1), 18.2)]
     return Bridge(x, y, connections, loads, supports)
+def createHowe(length, height, n):
+    dx = length/n
+    x = [i*dx for i in range(n+1)] + [i*dx for i in range(1,n)]
+    y = [0 for i in range(n+1)] + [height for i in range(1,n)]
+    connections = []
+    for i in range(1, n):
+        connections.append([i, i+n])
+    connections.append([0, n+1])
+    connections.append([n, 2*n-1])
+    for i in range(2, math.ceil(n/2)+1):
+        connections.append([i, i+n-1])
+    for i in range(n-2, n-math.ceil(n/2)-1, -1):
+        connections.append([i, i+n+1])
+    connections.extend([[i, i+1] for i in range(n)])
+    connections.extend([[i, i+1] for i in range(n+1, 2*n-1)])
+    loads = [(math.ceil((3/2)*n), 18.2)]
+    supports = [(0), (n)]
+    return Bridge(x, y, connections, loads, supports)
+def createWarren(length, height, n):
+    dx = length/n
+    x = [i*(dx/2) for i in range(2*n+1)]
+    y = [i%2*height for i in range(2*n+1)]
+    connections = [[i, i+1] for i in range(2*n)]
+    connections.extend([[i, i+2] for i in range(1, 2*n-1, 2)])
+    connections.extend([[i, i+2] for i in range(0, 2*n-1, 2)])
+    loads = [(n, 18.2)]
+    supports = [(0), (2*n)]
+    return Bridge(x, y, connections, loads, supports)
 
-b = Bridge([0,10,20,30,40,50,10,20,30,40], [0,0,0,0,0,0,15,15,15,15], [(0,6), (6,7), (7,8), (8,9), (9, 5), (4,5), (3,4), (2,3), (1,2), (0,1), (1,6), (2,7), (3,8), (4, 9), (1,7), (2,8), (3,7), (4,8)], [(2,9), (3,9)], [(0),(5)], unit='cm')
+
+#b = Bridge([0,10,20,30,40,50,10,20,30,40], [0,0,0,0,0,0,15,15,15,15], [(0,6), (6,7), (7,8), (8,9), (9, 5), (4,5), (3,4), (2,3), (1,2), (0,1), (1,6), (2,7), (3,8), (4, 9), (1,7), (2,8), (3,7), (4,8)], [(2,9), (3,9)], [(0),(5)], unit='cm')
+# b.show()
+# b.stress_test(0.1)
+# print(b.mass())
+b = createPratt(0.9, 0.12, 12)
+b2 = createKTruss(0.9, 0.12, 12)
+b3 = createHowe(0.9, 0.12, 12)
+b4 = createWarren(0.9, 0.12, 12)
 b.show()
-b.stress_test(0.01)
+b2.show()
+b3.show()
+b4.show()
